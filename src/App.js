3e5774bff11bba53f96d49bbfb7b62dbdbcd6000
definite2 <edefne878@gmail.com>
2021-08-06 20:10:54 +0300

    structure app and index file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { shallowEqual, useSelector } from "react-redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Layout from "./partials/layout";
+import { Logout } from "./pages/Auth/components";
+import { PrivateRoutes, PublicRoutes } from "./routes";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = ({ basename }) => {
+  const isAuthorized = useSelector(
+    ({ auth }) => auth.isAuthorized,
+    shallowEqual
   );
-}
 
+  return (
+    <BrowserRouter basename={basename}>
+      <Switch>
+        <Route path="/logout" component={Logout} />
+        {!isAuthorized ? (
+          <Route>
+            <PublicRoutes />
+          </Route>
+        ) : (
+          <>
+            <Layout>
+              <PrivateRoutes />
+            </Layout>
+          </>
+        )}
+      </Switch>
+    </BrowserRouter>
+  );
+};
 export default App;
