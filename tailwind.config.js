14224f5343320b26e7fbae7ac51e8a69ad026ff7
definite2 <edefne878@gmail.com>
2021-08-07 14:04:09 +0300

    update tailwind config, import styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,36 @@
+const colors = require("tailwindcss/colors");
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class',
+  important: true,
   theme: {
     extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.coolGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      green: colors.emerald,
+      purple: colors.violet,
+    },
+    borderColor: (theme) => ({
+      ...theme("colors"),
+      DEFAULT: theme("colors.gray.100","currentColor"),
+      primary: theme("colors.gray.200"),
+      danger: theme("colors.red.400"),
+      success: theme("colors.green.400"),
+    }),
   },
   variants: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: "class",
+    }),
+  ],
+};
