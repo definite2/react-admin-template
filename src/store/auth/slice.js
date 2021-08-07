543850715bccd96b032a34cc3d1d36768a3b0244
definite2 <edefne878@gmail.com>
2021-08-07 14:05:16 +0300

    update auth slice

--- src/store/auth/slice.js
+++ src/store/auth/slice.js
@@ -1,24 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  isAuthorized:false,//TODO remove this later implement accesToken
-  user: undefined,
-  accessToken:undefined,
-  error:false,
-  loading:false
+  isAuthorized: false, //TODO remove this later implement accesToken
+  user: { email: "" },
+  accessToken: undefined,
+  error: false,
+  loading: false,
 };
 
 export const authSlice = createSlice({
   name: "auth",
   initialState: initialState,
   reducers: {
-    login(state,action) {
+    login(state, action) {
+      const { email } = action.payload;
+      state.user.email = email;
       state.isAuthorized=true;
     },
     logout(state) {
       state.user = {};
-      state.accessToken="";
-      state.isAuthorized=false;
+      state.accessToken = "";
+      state.isAuthorized = false;
     },
   },
 });
