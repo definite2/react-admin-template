7c67bbcf89d47a279d0063602057571fa4abc06e
definite2 <edefne878@gmail.com>
2021-08-08 11:17:34 +0300

    add reviews api

---
+++ src/services/reviewsApi.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { REVIEWS_SERVER_URL } from "../constants";
+import { mockReviewsTableData } from "../mock/reviewsTableData";
+//this is real api of your
+export const reviewsApi = {
+  getReviews: (params) => axios.get(`${REVIEWS_SERVER_URL}`, { params }), //params for filtering
+};
+//this is mock service for demo purposes:
+export const mockGetReviewsService = (params) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      return resolve(mockReviewsTableData);
+    }, 500);
+  });
+};
