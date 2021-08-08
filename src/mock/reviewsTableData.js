591b48f88466f580f1720b9bfa02fa5cb4f18eaf
definite2 <edefne878@gmail.com>
2021-08-08 11:16:15 +0300

    add mock data for reviews table

---
+++ src/mock/reviewsTableData.js
@@ -0,0 +1,46 @@
+export const mockReviewsTableData = {
+  statusCode:200,
+  data: [
+    {
+      name: "Dr. Declan Schowalter",
+      comment:
+        "Qui ab magni. Tempore eos exercitationem ea. Voluptas vel tempore rerum quae eveniet voluptatem. Quasi tenetur velit et expedita ea cumque ipsam est amet.",
+
+      timeago: "a few seconds ago",
+      email: "declean@gmail.com",
+      date: "2021-07-26T03:55:37.43Z",
+      status: "handled",
+    },
+    {
+      name: "Dr. Carmen Will",
+      comment:
+        "Temporibus et occaecati. Asperiores officiis placeat harum fuga culpa tempora. Facere veritatis ullam fugit officia hic amet maxime error voluptatum. Modi et et necessitatibus non laborum sed dicta distinctio voluptatem.",
+
+      timeago: "an hour ago",
+      email: "carmen@gmail.com",
+      date: "2021-07-26T03:55:37.43Z",
+      status: "unhandled",
+    },
+    {
+      name: "Dr. Buddy Wilderman",
+      comment:
+        "Non veritatis commodi distinctio quisquam autem pariatur a. Accusamus animi natus exercitationem incidunt ad et atque. Sapiente vel necessitatibus omnis eum voluptates illum. Illum cumque eum illo eius molestiae id non. Et saepe et ut nulla. Id non in et commodi ullam.",
+
+      timeago: "2 hours ago",
+      email: "buddy@gmail.com",
+      date: "2021-07-26T03:55:37.43Z",
+      status: "handled",
+    },
+    {
+      name: "Dr. Baylee Handerson",
+      comment:
+        "Provident sit animi quia praesentium. Porro culpa eos labore quia necessitatibus.",
+
+      timeago: "3 hours ago",
+      email: "baylee@yahoo.com",
+      date: "2021-07-26T03:55:37.43Z",
+      status: "handled",
+    },
+  ],
+  totalPages:3
+};
