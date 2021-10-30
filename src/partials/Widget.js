16d3a782e5fe79ef366547fb5173159c334f8edf
definite2 <edefne878@gmail.com>
2021-10-30 12:28:15 +0300

    update widget component

--- src/partials/Widget.js
+++ src/partials/Widget.js
@@ -1,20 +1,23 @@
 import React from "react";
-import { Card, Image } from "semantic-ui-react";
-export const Widget = () => {
+import styled from "styled-components";
+import tw from "twin.macro";
+const IconWrapper = styled.div`
+  ${tw`rounded-full p-3 text-white mr-4`}
+`;
+export const Widget = ({ icon, title, value,iconColor }) => {
   return (
-    <Card className="w-full p-4 rounded-lg bg-white border border-gray-100 ">
-      <Card.Content>
-        <Image
-          floated="right"
-          size="mini"
-          src="/images/avatar/large/steve.jpg"
-        />
-        <Card.Header>Steve Sanders</Card.Header>
-        <Card.Meta>Friends of Elliot</Card.Meta>
-        <Card.Description>
-          Steve wants to add you to the group <strong>best friends</strong>
-        </Card.Description>
-      </Card.Content>
-    </Card>
+    <div className="w-full p-4 rounded-lg border border-gray-100 bg-gray-100 shadow-lg">
+      <div className="flex items-center justify-between">
+        <IconWrapper className={iconColor}>{icon}</IconWrapper>
+        <div className="flex flex-col">
+          <p className="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
+            {title}
+          </p>
+          <p className="text-lg font-semibold text-gray-700 dark:text-gray-200">
+            {value}
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
