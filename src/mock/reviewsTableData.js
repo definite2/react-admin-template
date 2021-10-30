e70d0fc894b95882d0ff11bd4a7fc59d39f8d606
definite2 <edefne878@gmail.com>
2021-10-30 12:28:34 +0300

    update mock data

--- src/mock/reviewsTableData.js
+++ src/mock/reviewsTableData.js
@@ -1,46 +1,46 @@
-export const mockReviewsTableData = {
-  statusCode:200,
+export const userTableData = {
+  statusCode: 200,
   data: [
     {
-      name: "Dr. Declan Schowalter",
+      username: "Dr. Declan Schowalter",
       comment:
         "Qui ab magni. Tempore eos exercitationem ea. Voluptas vel tempore rerum quae eveniet voluptatem. Quasi tenetur velit et expedita ea cumque ipsam est amet.",
 
-      timeago: "a few seconds ago",
+      comapny: "X Company",
       email: "declean@gmail.com",
-      date: "2021-07-26T03:55:37.43Z",
+      registerDate: "2021-07-26T03:55:37.43Z",
       status: "handled",
     },
     {
-      name: "Dr. Carmen Will",
+      username: "Dr. Carmen Will",
       comment:
         "Temporibus et occaecati. Asperiores officiis placeat harum fuga culpa tempora. Facere veritatis ullam fugit officia hic amet maxime error voluptatum. Modi et et necessitatibus non laborum sed dicta distinctio voluptatem.",
 
-      timeago: "an hour ago",
+      company: "Y company",
       email: "carmen@gmail.com",
-      date: "2021-07-26T03:55:37.43Z",
+      registerDate: "2021-07-26T03:55:37.43Z",
       status: "unhandled",
     },
     {
-      name: "Dr. Buddy Wilderman",
+      username: "Dr. Buddy Wilderman",
       comment:
         "Non veritatis commodi distinctio quisquam autem pariatur a. Accusamus animi natus exercitationem incidunt ad et atque. Sapiente vel necessitatibus omnis eum voluptates illum. Illum cumque eum illo eius molestiae id non. Et saepe et ut nulla. Id non in et commodi ullam.",
 
-      timeago: "2 hours ago",
+      comapny: "Z Company",
       email: "buddy@gmail.com",
-      date: "2021-07-26T03:55:37.43Z",
+      registerDate: "2021-07-26T03:55:37.43Z",
       status: "handled",
     },
     {
-      name: "Dr. Baylee Handerson",
+      username: "Dr. Baylee Handerson",
       comment:
         "Provident sit animi quia praesentium. Porro culpa eos labore quia necessitatibus.",
 
-      timeago: "3 hours ago",
+      company: "X Company",
       email: "baylee@yahoo.com",
-      date: "2021-07-26T03:55:37.43Z",
+      registerDate: "2021-07-26T03:55:37.43Z",
       status: "handled",
     },
   ],
-  totalPages:3
+  totalPages: 1,
 };
