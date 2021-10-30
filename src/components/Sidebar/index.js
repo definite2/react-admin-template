e0d1717dd42851f896437193123cffb486f42c27
definite2 <edefne878@gmail.com>
2021-10-30 12:32:49 +0300

    update sidebar component

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,13 +1,9 @@
 import React from "react";
-import { Icon, Menu, Segment, Sidebar as SUISidebar } from "semantic-ui-react";
+import { Icon, Menu, Sidebar as SUISidebar } from "semantic-ui-react";
 import { useSelector, shallowEqual, useDispatch } from "react-redux";
-import styled from "styled-components";
 import { setSideBarOpen } from "../../store/ui/actions";
-const Pushable = styled(SUISidebar.Pushable)`
-  margin-top: 0 !important;
-  border: none !important;
-`;
-
+import Input from "../FormItems/Input";
+import { NavLink,useLocation } from "react-router-dom";
 const Sidebar = (props) => {
   const dispatch = useDispatch();
   const handleClickMenu = () => {
@@ -18,38 +14,41 @@ const Sidebar = (props) => {
     shallowEqual
   );
   const { isSidebarOpen } = uistate;
+  const location = useLocation()
   return (
-    <Pushable as={Segment}>
+    <SUISidebar.Pushable>
       <SUISidebar
         as={Menu}
-        animation="overlay"
+        animation="uncover"
         icon="labeled"
         inverted
         vertical
-        visible={true}
+        visible
         width={isSidebarOpen ? "thin" : "very thin"}
       >
-        <Menu.Item as="a" onClick={handleClickMenu}>
-          <Icon name="bars" />
-        </Menu.Item>
-
-        <Menu.Item  as="a">
-          <Icon name="home" className={`${isSidebarOpen ? "" : "mr-5"}`}  />
-          <span className={`${isSidebarOpen ? "" : "hidden"}`}>Home</span>
+        <div
+          className="flex justify-end items-center text-white cursor-pointer h-12"
+          onClick={handleClickMenu}
+        >
+          <Icon name="bars" size="large" />
+        </div>
+        <hr />
+        <Menu.Item as="a">
+          <Icon name="table" className={`${isSidebarOpen ? "" : "mr-5"}`} />
+          <span className={`${isSidebarOpen ? "" : "hidden"}`}>Tables</span>
         </Menu.Item>
         <Menu.Item as="a">
-          <Icon name="gamepad" className={`${isSidebarOpen ? "" : "mr-5"}`}  />
-          <span className={`${isSidebarOpen ? "" : "hidden"}`}>Game</span>
+          <Icon name="chart bar" className={`${isSidebarOpen ? "" : "mr-5"}`} />
+          <span className={`${isSidebarOpen ? "" : "hidden"}`}>Charts</span>
         </Menu.Item>
         <Menu.Item as="a">
-          <Icon name="camera" className={`${isSidebarOpen ? "" : "mr-5"}`} />
-          <span className={`${isSidebarOpen ? "" : "hidden"}`}>
-            Channels
-          </span>
+          <Icon name="wpforms" className={`${isSidebarOpen ? "" : "mr-5"}`} />
+          <span className={`${isSidebarOpen ? "" : "hidden"}`}>Forms</span>
         </Menu.Item>
+        {isSidebarOpen && <Input icon="search" placeholder="Search..." />}
       </SUISidebar>
-      {props.children}
-    </Pushable>
+      <SUISidebar.Pusher>{props.children}</SUISidebar.Pusher>
+    </SUISidebar.Pushable>
   );
 };
 
